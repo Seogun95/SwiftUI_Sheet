3a36975894e6d7e3cf96299c6dba2e7574a3156d
SeonJungKim <ksj083895@gmail.com>
2021-05-10 22:52:57 +0500

    Initial Commit

---
+++ Shared/SwiftUI_SheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SheetApp.swift
+//  Shared
+//
+//  Created by Seogun Kim on 2021/05/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
